788a7341d9de7e191e64f1564a2d776ac6dda0c6
Arthur Oulès <69710623+Arthur-Oules@users.noreply.github.com>
2026-03-23 11:30:59 +0100

    Initial commit (2023-2025 version)

---
+++ functions/tidy_functions.R
@@ -0,0 +1,21 @@
+Get_sequence <- function(AC_number) {# Gets nucleotide sequences from Genbank Accession number
+  if (is.na(AC_number) == TRUE) {NA} else {
+    entrez_fetch(
+      db      = "nuccore",
+      id      = AC_number,
+      rettype = "fasta_cds_na",
+      retmode = "text"
+    ) |>
+      strsplit("]") |>
+      unlist() |>
+      tail(1) |>
+      gsub(pattern = "\n", replacement = "",  x = _)
+  }
+}
+
+write_to_fasta <- function(tib, path) {# Writes two column tib as a .fa file
+  # Expected tibble structure is:
+  # fasta name | sequence
+  tib$accession <- paste0(">", tib$accession)
+  c(rbind(tib$accession, tib$sequences)) |> write(file = path)
+}
